d483dada7c0c1c28d34cd0b91c1b87cd8259a663
trupti1011 <Trupti.Patil@intelenetglobal.com>
2020-06-02 12:59:33 +0530

    Adding files

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Persistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+
+        }
+        public DbSet<Values> Values { get; set; }
+    }
+}
